b1c29497bfb48f57b9955afdf00db38fa9274f5f
Shreyas578 <nikamsarita21@gmail.com>
2025-11-13 09:45:08 +0530

    Update hardhat.config.ts

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -20,13 +20,32 @@ const config: HardhatUserConfig = {
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
     },
+    moonbase: {
+      url: process.env.MOONBASE_URL || "https://rpc.api.moonbase.moonbeam.network",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1287,
+      gasPrice: 1000000000,
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      moonbaseAlpha: process.env.MOONSCAN_API_KEY || "",
+    },
+    customChains: [
+      {
+        network: "moonbaseAlpha",
+        chainId: 1287,
+        urls: {
+          apiURL: "https://api-moonbase.moonscan.io/api",
+          browserURL: "https://moonbase.moonscan.io",
+        },
+      },
+    ],
   },
   paths: {
     sources: "./contracts",
